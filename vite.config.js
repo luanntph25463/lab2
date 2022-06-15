0c1d157a9f990fd56ec87bd5f10769d7f6c5653b
luanntph25463 <105334790+luanntph25463@users.noreply.github.com>
2022-06-15 12:21:59 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+
+/* eslint-disable import/no-extraneous-dependencies */
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "./index.html"),
+
+            },
+        },
+    },
+});
